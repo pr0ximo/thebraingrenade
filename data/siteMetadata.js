847e2d374ab7a51113b778c2697c51b6604f610e
Devin Kim <devinkim@Devins-MacBook-Air.local>
2023-03-28 17:32:08 -0700

    adding giscus discussions

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,13 +5,13 @@ const siteMetadata = {
   description: 'Documenting my journey around the sun',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://thebraingrenade.com',
+  siteRepo: 'https://github.com/pr0ximo/thebraingrenade',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'dwk1987@gmail.com',
-  github: 'https://github.com',
+  github: 'https://github.com/pr0ximo',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
@@ -21,8 +21,8 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
+    plausibleDataDomain: thebraingrenade.com, // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: true, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
